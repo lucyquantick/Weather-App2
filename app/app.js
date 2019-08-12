d2534187c7cda74b85e03992c06bc98b6fed81bc
Lucy Quantick <lucyquantick@gmail.com>
2019-08-12 20:31:10 +0100

    add routeParams for number of results returned and re-added convertDate function

--- app/app.js
+++ app/app.js
@@ -12,7 +12,12 @@ weatherApp.config(function ($routeProvider) {
 		.when('/forecast', {
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
+		})
+		.when('/forecast/:days', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
 		});
+
 });
 
 // SERVICES
--- app/controllers/forecastController.js
+++ app/controllers/forecastController.js
@@ -1,14 +1,20 @@
-﻿weatherApp.controller('forecastController', ['$scope', '$resource', '$log', 'cityService', function ($scope, $resource, $log, cityService) {
+﻿weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', '$log', 'cityService', function ($scope, $resource, $routeParams, $log, cityService) {
 
 	$scope.city = cityService.city;
+
+	$scope.results = $routeParams.results || 6;
 
 	$scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast",
 		{ get: { method: "JSONP" } });
 
-	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, APPID: "b1887bf2fd62dadae7cbe58ba9ea08f1" });	
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.results, APPID: "b1887bf2fd62dadae7cbe58ba9ea08f1" });	
 
 	$scope.convertToCelsius = function (degK) {
 		return Math.round(degK - 273);
+	};
+
+	$scope.convertToDate = function (dt) {
+		return new Date(dt * 1000);
 	};
 
 	//$log.log($scope.weatherResult);
