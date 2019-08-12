2bbac7983ee4d31ccf9d8192826b19ca0ed9abb9
Lucy Quantick <lucyquantick@gmail.com>
2019-08-12 20:21:33 +0100

    accessing api and returning temperatures and dates to page

--- app/controllers/forecastController.js
+++ app/controllers/forecastController.js
@@ -1,5 +1,16 @@
-﻿weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+﻿weatherApp.controller('forecastController', ['$scope', '$resource', '$log', 'cityService', function ($scope, $resource, $log, cityService) {
 
 	$scope.city = cityService.city;
+
+	$scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast",
+		{ get: { method: "JSONP" } });
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, APPID: "b1887bf2fd62dadae7cbe58ba9ea08f1" });	
+
+	$scope.convertToCelsius = function (degK) {
+		return Math.round(degK - 273);
+	};
+
+	//$log.log($scope.weatherResult);
 
 }]);
